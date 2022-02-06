2d3e80420cb28064a319854354f6d7ffe601c88d
Ritik raj <ritikraj2884@gmail.com>
2022-02-06 17:52:33 +0530

    Time: 0 ms (100.00%), Space: 6.5 MB (52.03%) - LeetHub

---
+++ 58-length-of-last-word/58-length-of-last-word.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+        int flag = 0, c=0;
+        int len  =s.length();
+        while((len--)>0){
+            if(s[len]== ' '){
+                if(flag == 1) break;
+                continue;    
+            }
+            else{
+                c++;
+                flag  =1;
+            } 
+        }
+        return c;  
+    }
+};
