078c472fe06af28ae2711caa91efd4f643ba902e
Ritik raj <ritikraj2884@gmail.com>
2022-02-07 23:43:57 +0530

    Time: 80 ms (33.38%), Space: 35 MB (5.01%) - LeetHub

--- 680-valid-palindrome-ii/680-valid-palindrome-ii.cpp
+++ 680-valid-palindrome-ii/680-valid-palindrome-ii.cpp
@@ -1,12 +1,12 @@
 class Solution {
 public:
-    int indexOfdis = 0;
+    int indexOfdisruption = 0;
     //function to check if the string is palindrome or not
     bool check(string s){
         int size = s.length();
         for(int i=0;i<size;i++){
             if(s[i] != s[size-i-1]){
-                indexOfdis = i;
+                indexOfdisruption = i;
                 return false;
             }
         }
@@ -19,11 +19,9 @@ public:
         }
         else{
             string temp =s;
-            string s1 = s.erase(indexOfdis,1);
-            cout<<"s1: "<<s1<<endl;
-            string s2 = temp.erase(temp.length()-indexOfdis-1, 1);
-            cout<<"s2: "<<s2<<endl;
-            //remove the size-i-1 index
+            string s1 = s.erase(indexOfdisruption,1);
+            string s2 = temp.erase(temp.length()-indexOfdisruption-1, 1);
+            //remove the (size-i-1) index from string
             if(check(s1) || check(s2)){
                 return true;
             }
