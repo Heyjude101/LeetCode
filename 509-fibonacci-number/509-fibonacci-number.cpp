6e53eca3640872b4a1c227fba783d4f38813fa7c
Ritik raj <ritikraj2884@gmail.com>
2022-02-18 00:39:41 +0530

    Time: 0 ms (100.00%), Space: 6 MB (16.71%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.cpp
+++ 509-fibonacci-number/509-fibonacci-number.cpp
@@ -1,15 +1,35 @@
 class Solution {
 public:
-    int f(int n , vector<int> &dp){
-        //base case
-        if(n==0) return 0;
-        if(n ==1) return 1;
-        if(dp[n] != -1) return dp[n];
-        return dp[n] = f(n-1 , dp) + f(n-2 , dp);   
+    //Now convert to tabulated form
+    int f(int n ,vector<int> &dp){
+        dp.push_back(0);
+        dp.push_back(1);
+        cout<<dp[0];
+        cout<<dp[1];
+        for(int i=2;i<=n;i++){
+            cout<<"in lopp";
+            dp.push_back(dp[i-1] + dp[i-2]);
+            cout<<"after loop";
+        }
+        return dp[n];
     }
+    
+    
+    
+    
+    
+    
+    
+    // int f(int n , vector<int> &dp){
+    //     //base case
+    //     if(n==0) return 0;
+    //     if(n ==1) return 1;
+    //     if(dp[n] != -1) return dp[n];
+    //     return dp[n] = f(n-1 , dp) + f(n-2 , dp);   
+    // }
     int fib(int n) {
         //recursive dp technique
-        vector<int> dp(n+1 ,-1);
+        vector<int> dp;
         return f(n , dp);
         // if(n==0) return 0;
         // if(n==1) return 1;
