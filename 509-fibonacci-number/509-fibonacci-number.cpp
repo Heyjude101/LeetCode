06f669de2b5c15a0720b6b8617af38295e7b0fb0
Ritik raj <ritikraj2884@gmail.com>
2022-02-18 00:45:59 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (39.52%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.cpp
+++ 509-fibonacci-number/509-fibonacci-number.cpp
@@ -1,17 +1,17 @@
 class Solution {
 public:
-    //Now convert to tabulated form
-    int f(int n ,vector<int> &dp){
-        dp.push_back(0);
-        dp.push_back(1);
-        cout<<dp[0];
-        cout<<dp[1];
+    //Now optimize space to O(1)
+     int f(int n){
+        int prev = 1;
+        int prev2 = 0;
+         int ans = n;
         for(int i=2;i<=n;i++){
-            cout<<"in lopp";
-            dp.push_back(dp[i-1] + dp[i-2]);
-            cout<<"after loop";
+            // dp.push_back(dp[i-1] + dp[i-2]);
+            ans = prev + prev2;
+            prev2 = prev;
+            prev = ans;
         }
-        return dp[n];
+        return ans;
     }
     
     
@@ -19,7 +19,17 @@ public:
     
     
     
+    //Now convert to tabulated form
+    // int f(int n ,vector<int> &dp){
+    //     dp.push_back(0);
+    //     dp.push_back(1);
+    //     for(int i=2;i<=n;i++){
+    //         dp.push_back(dp[i-1] + dp[i-2]);
+    //     }
+    //     return dp[n];
+    // }
     
+
     // int f(int n , vector<int> &dp){
     //     //base case
     //     if(n==0) return 0;
@@ -29,8 +39,8 @@ public:
     // }
     int fib(int n) {
         //recursive dp technique
-        vector<int> dp;
-        return f(n , dp);
+        // vector<int> dp;
+        return f(n);
         // if(n==0) return 0;
         // if(n==1) return 1;
         // return dp[n] = ib(n-1) + fib(n-2);  
