6cb2c713c4a6f91e6827baf3cbb8b345a1c3399d
Ritik raj <ritikraj2884@gmail.com>
2022-02-18 01:39:30 +0530

    Time: 0 ms (100.00%), Space: 6 MB (36.96%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.cpp
+++ 70-climbing-stairs/70-climbing-stairs.cpp
@@ -1,10 +1,18 @@
 class Solution {
 public:
-    int f(int n , array<int , 46> &dp){
-        if(n<=2) return n;
-        if(dp[n] != -1) return dp[n];
-        return dp[n] = f(n-1 , dp) + f(n-2 , dp);
+    int f(int n, array<int , 46> &dp){
+        dp[1] = 1;
+        dp[2] =2;
+        for(int i=3; i<=n; i++){
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+        return dp[n];
     }
+    // int f(int n , array<int , 46> &dp){
+    //     if(n<=2) return n;
+    //     if(dp[n] != -1) return dp[n];
+    //     return dp[n] = f(n-1 , dp) + f(n-2 , dp);
+    // }
     int climbStairs(int n) {
         array<int , 46> dp;
         dp.fill(-1);
