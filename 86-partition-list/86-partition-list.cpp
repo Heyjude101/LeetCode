0f8cb6c1451934c5bd63cd671f003c336d8ba44b
Ritik raj <ritikraj2884@gmail.com>
2022-02-26 04:48:00 +0530

    Time: 4 ms (88.21%), Space: 10.3 MB (31.86%) - LeetHub

--- 86-partition-list/86-partition-list.cpp
+++ 86-partition-list/86-partition-list.cpp
@@ -11,31 +11,66 @@
 class Solution {
 public:
     ListNode* partition(ListNode* head, int x) {
-        if(head == NULL or head->next == NULL) return head;
-        int p =201;
-        while(p--){
-            ListNode* i = head;
-            ListNode* j = head->next;
-            int flag = 1;
-        while(j!= NULL){
-            if(j->val < x){
-                cout<<"*";
-                flag =0;
-                if(i->val >= x){
-                int temp = i->val;
-                i->val = j->val;
-                j->val = temp;
-                }
+        //solve using two pointer approach in single pass
+        ListNode* L = new ListNode(0);
+        ListNode* R = new ListNode(0);
+        ListNode* LH = L;
+        ListNode* RH = R;
+        while(head){
+            if(head->val < x){
+                // L = L->next = head;
+                L->next = head;
+                L=L->next;
             }
-            i = i->next;
-            j = j->next;  
-        }
-            if(flag == 1){
-                cout<<"flag ==1 reached";
-                break;
+            else{
+                R->next = head;
+                R = R->next;
+                // R = R->next = head;
             }
+            head = head->next;
         }
-        return head;
+        R->next = NULL;
+        L->next = RH->next;
+        return LH->next;
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        // if(head == NULL or head->next == NULL) return head;
+        // int p =201;
+        // while(p--){
+        //     ListNode* i = head;
+        //     ListNode* j = head->next;
+        //     int flag = 1;
+        // while(j!= NULL){
+        //     if(j->val < x){
+        //         cout<<"*";
+        //         flag =0;
+        //         if(i->val >= x){
+        //         int temp = i->val;
+        //         i->val = j->val;
+        //         j->val = temp;
+        //         }
+        //     }
+        //     i = i->next;
+        //     j = j->next;  
+        // }
+        //     if(flag == 1){
+        //         cout<<"flag ==1 reached";
+        //         break;
+        //     }
+        // }
+        // return head;
         
     }
 };
