3eb7c01ad76b30510d0c61e20835d0d45da5a89d
Ritik raj <ritikraj2884@gmail.com>
2022-02-26 01:10:10 +0530

    Time: 0 ms (100.00%), Space: 10.7 MB (75.60%) - LeetHub

--- 19-remove-nth-node-from-end-of-list/19-remove-nth-node-from-end-of-list.cpp
+++ 19-remove-nth-node-from-end-of-list/19-remove-nth-node-from-end-of-list.cpp
@@ -10,50 +10,65 @@
  */
 class Solution {
 public:
-    int countNodes(ListNode* x){
-        ListNode* curr = x;
-        int c=0;
-        while(curr!=NULL){
-            c++;
-            curr=curr->next;
-        }
-        return c;
-    }
+    //function to count the number of nodes in the linked list
+    // int countNodes(ListNode* x){
+    //     ListNode* curr = x;
+    //     int c=0;
+    //     while(curr!=NULL){
+    //         c++;
+    //         curr=curr->next;
+    //     }
+    //     return c;
+    // }
     ListNode* removeNthFromEnd(ListNode* head, int n) {
-        int req = countNodes(head) - n + 1;
-        // cout<<head->val;
+        //solving in 1 iteration
+        //first we send the second pointer to the nth place 
+        ListNode* i = head;
+        ListNode* j = head;
+       while(n--){
+           j=j->next;
+       }
+        if(j == NULL){
+            return head->next;
+        }
+        while(j->next!=NULL){
+            i = i->next;
+            j = j->next;
+        }
+        i->next = i->next->next;
+        return head;
+        
+        
+        
+
+        // int req = countNodes(head) - n + 1;
+        // cout<<req;
+        // ListNode* temp = head;
+        // if(req ==1){
+        //     //remove from front
+        //     cout<<"RFF";
+        //     return temp->next;
+        // }
+        // else if(req == countNodes(head)){
+        //     //remove from last
+        //     cout<<"RFL";
+        //     for(int i=0;i<req-2;i++){
+        //     temp = temp->next;   
+        //     }
+        //     temp->next = NULL;
+        // }
+        // else{
+        //     //remove from middle
+        //     cout<<"RFM";
+        //     for(int i=0;i<req-2;i++){
+        //     cout<<"* ";
+        //     temp = temp->next; 
+        //     }
+        //     cout<<head->val;
         // cout<<head->next->val;
         // cout<<head->next->next->val;
-        // cout<<head->next->next->next->val;
-        // cout<<head->next->next->next->next->val;
-        cout<<req;
-        ListNode* temp = head;
-        if(req ==1){
-            //remove from front
-            cout<<"RFF";
-            return temp->next;
-        }
-        else if(req == countNodes(head)){
-            //remove from last
-            cout<<"RFL";
-            for(int i=0;i<req-2;i++){
-            temp = temp->next;   
-            }
-            temp->next = NULL;
-        }
-        else{
-            //remove from middle
-            cout<<"RFM";
-            for(int i=0;i<req-2;i++){
-            cout<<"* ";
-            temp = temp->next; 
-            }
-            cout<<head->val;
-        cout<<head->next->val;
-        cout<<head->next->next->val;
-            temp->next = temp->next->next; 
-            // return temp;
-        }
-        return head;
+        //     temp->next = temp->next->next; 
+        // }
+        // return head;
     }
 };
