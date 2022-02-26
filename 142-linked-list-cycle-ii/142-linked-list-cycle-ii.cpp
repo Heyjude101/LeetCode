8e8c9180d6fe0319b4f602faa8d523a5f8dfc371
Ritik raj <ritikraj2884@gmail.com>
2022-02-27 02:44:37 +0530

    Time: 7 ms (90.98%), Space: 7.4 MB (99.34%) - LeetHub

--- 142-linked-list-cycle-ii/142-linked-list-cycle-ii.cpp
+++ 142-linked-list-cycle-ii/142-linked-list-cycle-ii.cpp
@@ -9,29 +9,70 @@
 class Solution {
 public:
     ListNode *detectCycle(ListNode *head) {
-//         ListNode *slow = head, *fast = head;
-        
-//         while (fast && fast->next) {
-//             slow = slow->next;
-//             fast = fast->next->next;
-//             if (slow == fast) break;
-//         }
-//         if (!(fast && fast->next)) return NULL;
-//         while (head != slow) {
-//             head = head->next;
-//             slow = slow->next;
-//         }
-//         return head;
-        
-        
-        unordered_map<ListNode*  , bool> m;
-        while(head!=NULL){
-            if(m.count(head)){
-                return head;
-            }
-            m[head] = true;
-            head = head->next;
+        //using 2 pointers
+        //arrive at a node inside the cycle
+        ListNode* slow = head;
+        ListNode* fast = head;
+        while(fast and fast->next){
+            slow = slow->next;
+            fast = fast->next->next;
+            if(slow == fast) break;
         }
-        return NULL;
+        if(fast == NULL or fast->next == NULL) return NULL;
+        //count the nodes in the loop
+        slow = slow->next;
+        int count =1;
+        while(fast != slow){
+            count++;
+            slow = slow->next;
+        }
+        ListNode* a =head;
+        ListNode* b = head;
+        while(count--){
+            b = b->next;
+        }
+        while(a!=b){
+            a = a->next;
+            b =b->next;
+        }
+        
+        return a;
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        // ListNode *slow = head, *fast = head;
+        // while (fast && fast->next) {
+        //     slow = slow->next;
+        //     fast = fast->next->next;
+        //     if (slow == fast) break;
+        // }
+        // if (!(fast && fast->next)) return NULL;
+        // while (head != slow) {
+        //     head = head->next;
+        //     slow = slow->next;
+        // }
+        // return head;
+        
+        //using maps
+        // unordered_map<ListNode*  , bool> m;
+        // while(head!=NULL){
+        //     if(m.count(head)){
+        //         return head;
+        //     }
+        //     m[head] = true;
+        //     head = head->next;
+        // }
+        // return NULL;
     }
 };
