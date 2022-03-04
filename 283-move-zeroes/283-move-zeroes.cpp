69c83b11f638144af588636a28e87052075d84bb
Ritik raj <ritikraj2884@gmail.com>
2022-03-04 05:39:45 +0530

    Time: 25 ms (74.71%), Space: 19.2 MB (58.52%) - LeetHub

--- 283-move-zeroes/283-move-zeroes.cpp
+++ 283-move-zeroes/283-move-zeroes.cpp
@@ -1,17 +1,19 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        int len = nums.size();
-        int pos = 0;
-        for(auto it: nums){
-            if(it != 0) {
-                nums[pos] = it;
-                pos++;
-            }
-        }
-        for(int i=pos;i<len;i++){
-            nums[i] = 0;
-        }
+        //one liner STL
+        fill(remove(nums.begin() , nums.end() , 0) , nums.end() , 0);
+        // int len = nums.size();
+        // int pos = 0;
+        // for(auto it: nums){
+        //     if(it != 0) {
+        //         nums[pos] = it;
+        //         pos++;
+        //     }
+        // }
+        // for(int i=pos;i<len;i++){
+        //     nums[i] = 0;
+        // }
 //         for(int i=0;i<len-1;i++){
 //             for(int j=i;j<len-1;j++){
 //             if(nums[i] == 0 && nums[j+1]!=0){
