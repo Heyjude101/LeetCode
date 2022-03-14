3ab298c3bc8d4577059d07540028b901073ea786
Ritik raj <ritikraj2884@gmail.com>
2022-03-14 20:51:16 +0530

    Time: 104 ms (18.92%), Space: 9.7 MB (45.22%) - LeetHub

--- 1089-duplicate-zeros/1089-duplicate-zeros.cpp
+++ 1089-duplicate-zeros/1089-duplicate-zeros.cpp
@@ -1,18 +1,36 @@
 class Solution {
 public:
     void duplicateZeros(vector<int>& arr) {
-        int len = arr.size();
-        for(int i=0;i<len;i++){           
-            if(arr[0] == 0){
-                arr.push_back(arr[0]);
-                arr.push_back(arr[0]);
-                arr.erase(arr.begin());
+        // int len = arr.size();
+        // for(int i=0;i<len;i++){           
+        //     if(arr[0] == 0){
+        //         arr.push_back(arr[0]);
+        //         arr.push_back(arr[0]);
+        //         arr.erase(arr.begin());
+        //     }
+        //     else{
+        //         arr.push_back(arr[0]);
+        //         arr.erase(arr.begin());
+        //     } 
+        // }
+        // arr.erase(arr.begin()+len , arr.end());
+        //Optimal solution
+        //find the length
+        int len  = arr.size();
+        int j=0;
+        //first loop
+        for(int i=0;i<len;i++){
+            //check if the element is zero
+            if(arr[i] == 0){
+            //run another loop from backwards in the required range
+                for(j=arr.size()-1;j>=i+1;j--)
+                {
+                arr[j] = arr[j-1];
             }
-            else{
-                arr.push_back(arr[0]);
-                arr.erase(arr.begin());
-            } 
+                //here J will have garbage value if we don't initialize it to 0
+            i= j+1;
+            }
         }
-        arr.erase(arr.begin()+len , arr.end());
+        
     }
 };
