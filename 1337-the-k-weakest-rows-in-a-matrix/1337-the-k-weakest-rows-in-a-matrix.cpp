34fbc7705d14b822d9c5853284cb401d47af4ac8
Ritik raj <ritikraj2884@gmail.com>
2022-03-27 09:43:11 +0530

    Time: 12 ms (83.53%), Space: 10.4 MB (91.37%) - LeetHub

--- 1337-the-k-weakest-rows-in-a-matrix/1337-the-k-weakest-rows-in-a-matrix.cpp
+++ 1337-the-k-weakest-rows-in-a-matrix/1337-the-k-weakest-rows-in-a-matrix.cpp
@@ -3,14 +3,14 @@ public:
     vector<int> kWeakestRows(vector<vector<int>>& mat, int k) {
         vector<int> ans_sample;
         int r=mat.size() , c = mat[0].size();
-        for(int i=r-1;i>=0;i--){
-            if(mat[i][c-1]==1){
-                ans_sample.push_back(i);
-            }
-        }
-        for(int j=c-2;j>=0;j--){
+        // for(int i=r-1;i>=0;i--){
+        //     if(mat[i][c-1]==1){
+        //         ans_sample.push_back(i);
+        //     }
+        // }
+        for(int j=c-1;j>=0;j--){
             for(int i=r-1;i>=0;i--){
-                if(mat[i][j] == 1 && mat[i][j+1] ==0){
+                if((j== c-1 or mat[i][j+1] ==0) &&  mat[i][j] == 1){
                     ans_sample.push_back(i);
                 }
             }
@@ -19,7 +19,7 @@ public:
             
             if(mat[i][0]==0) ans_sample.push_back(i);
         }
-        reverse(ans_sample.begin() , ans_sample.end());
-        return vector<int>(ans_sample.begin() , ans_sample.begin()+k);
+        // reverse(ans_sample.begin() , ans_sample.end());
+        return vector<int>(ans_sample.rbegin() , ans_sample.rbegin()+k);
     }
 };
