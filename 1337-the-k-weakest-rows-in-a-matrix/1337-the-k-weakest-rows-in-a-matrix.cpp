3fe87538de45cd08b8ac282090c8b9c35a3a2c5a
Ritik raj <ritikraj2884@gmail.com>
2022-03-27 10:44:24 +0530

    Time: 7 ms (98.92%), Space: 10.7 MB (13.11%) - LeetHub

--- 1337-the-k-weakest-rows-in-a-matrix/1337-the-k-weakest-rows-in-a-matrix.cpp
+++ 1337-the-k-weakest-rows-in-a-matrix/1337-the-k-weakest-rows-in-a-matrix.cpp
@@ -1,25 +1,65 @@
 class Solution {
 public:
     vector<int> kWeakestRows(vector<vector<int>>& mat, int k) {
-        vector<int> ans_sample;
-        int r=mat.size() , c = mat[0].size();
-        // for(int i=r-1;i>=0;i--){
-        //     if(mat[i][c-1]==1){
-        //         ans_sample.push_back(i);
-        //     }
-        // }
-        for(int j=c-1;j>=0;j--){
-            for(int i=r-1;i>=0;i--){
-                if((j== c-1 or mat[i][j+1] ==0) &&  mat[i][j] == 1){
-                    ans_sample.push_back(i);
-                }
+        set<pair<int,int> > s;
+        vector<int> ans;
+        for(int i=0;i<mat.size();i++){
+            s.insert({accumulate(mat[i].begin() , mat[i].end(),0) , i});
+        }
+        int c =1;
+        for(auto it= s.begin(); it!= s.end();it++){
+            if(c<=k){
+                ans.push_back(it->second);
+                c++;
+            }
+            else{
+                break;
             }
         }
-        for(int i=r-1;i>=0;i--){
+        return ans;
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+//         vector<int> ans_sample;
+//         int r=mat.size() , c = mat[0].size();
+//         // for(int i=r-1;i>=0;i--){
+//         //     if(mat[i][c-1]==1){
+//         //         ans_sample.push_back(i);
+//         //     }
+//         // }
+//         for(int j=c-1;j>=0;j--){
+//             for(int i=r-1;i>=0;i--){
+//                 if((j== c-1 or mat[i][j+1] ==0) &&  mat[i][j] == 1){
+//                     ans_sample.push_back(i);
+//                 }
+//             }
+//         }
+//         for(int i=r-1;i>=0;i--){
             
-            if(mat[i][0]==0) ans_sample.push_back(i);
-        }
-        // reverse(ans_sample.begin() , ans_sample.end());
-        return vector<int>(ans_sample.rbegin() , ans_sample.rbegin()+k);
+//             if(mat[i][0]==0) ans_sample.push_back(i);
+//         }
+//         // reverse(ans_sample.begin() , ans_sample.end());
+//         return vector<int>(ans_sample.rbegin() , ans_sample.rbegin()+k);
     }
 };
