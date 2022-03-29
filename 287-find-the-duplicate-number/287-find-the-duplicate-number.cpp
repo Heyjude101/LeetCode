a0ab9613eb048a726daf7c0b910a963757d95188
Ritik raj <ritikraj2884@gmail.com>
2022-03-29 23:02:08 +0530

    Time: 52 ms (99.98%), Space: 61 MB (99.87%) - LeetHub

--- 287-find-the-duplicate-number/287-find-the-duplicate-number.cpp
+++ 287-find-the-duplicate-number/287-find-the-duplicate-number.cpp
@@ -1,5 +1,10 @@
 class Solution {
 public:
+    Solution() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+  }
     int findDuplicate(vector<int>& nums) {
         int slow = nums[0];
         int fast = nums[0];
