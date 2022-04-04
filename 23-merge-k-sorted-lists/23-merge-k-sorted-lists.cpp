74475a7a6f33fbb19d54a3976c190b3a65c90c2f
Ritik raj <ritikraj2884@gmail.com>
2022-04-04 23:29:14 +0530

    Time: 32 ms (62.72%), Space: 13.2 MB (77.59%) - LeetHub

--- 23-merge-k-sorted-lists/23-merge-k-sorted-lists.cpp
+++ 23-merge-k-sorted-lists/23-merge-k-sorted-lists.cpp
@@ -10,51 +10,87 @@
  */
 class Solution {
 public:
-    ListNode* conquer(ListNode* &h1 , ListNode* &h2){
-        if(h1==NULL) return h2;
-        if(h2 == NULL) return h1;
-        ListNode* s = new ListNode(0);
-        if(h1->val < h2->val){
-            s = h1;
-            h1 = h1->next;
+    // ListNode* conquer(ListNode* &h1 , ListNode* &h2){
+    //     if(h1==NULL) return h2;
+    //     if(h2 == NULL) return h1;
+    //     ListNode* s = new ListNode(0);
+    //     if(h1->val < h2->val){
+    //         s = h1;
+    //         h1 = h1->next;
+    //     }
+    //     else{
+    //         s = h2;
+    //         h2 = h2->next;
+    //     }
+    //     //saved the head
+    //     ListNode* ans = s;
+    //     while(h1 && h2){
+    //         if(h1->val < h2->val){
+    //             s->next = h1;
+    //             s = h1;
+    //             h1 = h1->next;
+    //         }
+    //         else{
+    //             s->next = h2;
+    //             s = h2;
+    //             h2 = h2->next;
+    //         }  
+    //     }
+    //     if(h1){
+    //         s->next = h1;
+    //     }
+    //     if(h2){
+    //         s->next = h2;  
+    //     }
+    //     return ans;     
+    // }
+    // ListNode* divide(vector<ListNode*> &lists , int start , int end){
+    //     if(start ==  end) return lists[start];
+    //     int mid = (start + end)/2;
+    //     ListNode* left = divide(lists , start , mid);
+    //     ListNode* right = divide(lists , mid+1 , end);
+    //     return conquer(left , right);
+    // }
+    
+    struct mycmp{
+        bool operator()(ListNode* a, ListNode* b){
+            return a->val > b->val;
         }
-        else{
-            s = h2;
-            h2 = h2->next;
+    };
+    ListNode* mergeKLists(vector<ListNode*>& lists) {
+        
+        priority_queue<ListNode* ,  vector<ListNode*> , mycmp> pq;
+        for(int i=0;i<lists.size();i++){
+            if(lists[i]!=NULL){
+                pq.push(lists[i]);
+            }
         }
-        //saved the head
-        ListNode* ans = s;
-        while(h1 && h2){
-            if(h1->val < h2->val){
-                s->next = h1;
-                s = h1;
-                h1 = h1->next;
+        ListNode* ans = NULL;
+        ListNode* tail = NULL;
+        
+        while(!pq.empty()){
+            ListNode* temp = pq.top();
+            pq.pop();
+            if(ans == NULL){
+                ans = tail = temp;
             }
             else{
-                s->next = h2;
-                s = h2;
-                h2 = h2->next;
-            }  
+                tail->next = temp;
+                tail = temp;
+            }
+            if(temp->next != NULL){
+                pq.push(temp->next);
+            }
         }
-        if(h1){
-            s->next = h1;
-        }
-        if(h2){
-            s->next = h2;  
-        }
-        return ans;     
-    }
-    ListNode* divide(vector<ListNode*> &lists , int start , int end){
-        if(start ==  end) return lists[start];
-        int mid = (start + end)/2;
-        ListNode* left = divide(lists , start , mid);
-        ListNode* right = divide(lists , mid+1 , end);
-        return conquer(left , right);
-    }
-    ListNode* mergeKLists(vector<ListNode*>& lists) {
-        //basic checks
-        if(lists.size() == 0) return NULL;
-        if(lists.size() == 1) return lists[0];
-        return divide(lists , 0, lists.size()-1);
+        
+        return ans;
+        
+
+        
+        
+        // //basic checks
+        // if(lists.size() == 0) return NULL;
+        // if(lists.size() == 1) return lists[0];
+        // return divide(lists , 0, lists.size()-1);
     }
 };
