6c040f8dae9bbbb2c52ea1e4079a24140568ec8f
Ritik raj <ritikraj2884@gmail.com>
2022-04-04 19:23:19 +0530

    Time: 556 ms (99.69%), Space: 180.1 MB (57.49%) - LeetHub

--- 1721-swapping-nodes-in-a-linked-list/1721-swapping-nodes-in-a-linked-list.cpp
+++ 1721-swapping-nodes-in-a-linked-list/1721-swapping-nodes-in-a-linked-list.cpp
@@ -11,28 +11,46 @@
 class Solution {
 public:
     ListNode* swapNodes(ListNode* head, int k) {
-        int nodes =0;
-        ListNode* temp = head;
-        while(temp != NULL){
-            temp = temp->next;
-            nodes++;
+        //Optimal one pass
+        ListNode* p1 = head;
+        ListNode* p2 = head;
+        ListNode* lead = head;
+        int count = 1;
+        while(lead != NULL){
+            if(count<k){
+                p1 =  p1->next;
+            }
+            if(count>k){
+                p2 = p2->next;
+            }
+            lead = lead->next;
+            count++;
         }
-        int fromFront = k-1;
-        int fromBack = nodes - k;
-        ListNode* temp2 = head;
-                ListNode* temp3 = head;
-
-        while(fromBack--){
-            temp2 = temp2->next;
-        }
-        while(fromFront--){
-            temp3 = temp3->next;
-        }
-        
-        swap(temp3->val , temp2->val);
-        
+        swap(p1->val  , p2->val);
         return head;
- 
         
+        
+        //2 pass method ( We find the length and then do our stuff)
+//         int nodesCount =0;
+//         ListNode* temp = head;
+//         while(temp != NULL){
+//             temp = temp->next;
+//             nodesCount++;
+//         }
+//         int fromFront = k-1;
+//         int fromBack = nodesCount - k;
+//         ListNode* temp2 = head;
+//         ListNode* temp3 = head;
+
+//         while(fromBack--){
+//             temp2 = temp2->next;
+//         }
+//         while(fromFront--){
+//             temp3 = temp3->next;
+//         }
+        
+//         swap(temp3->val , temp2->val);
+
+//         return head; 
     }
 };
