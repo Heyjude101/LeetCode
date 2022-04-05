ad85fce56ffc83ef5cc6a3a7ec2d1935b0561e1b
Ritik raj <ritikraj2884@gmail.com>
2022-04-05 13:07:53 +0530

    Time: 27 ms (98.86%), Space: 27.5 MB (63.56%) - LeetHub

--- 704-binary-search/704-binary-search.cpp
+++ 704-binary-search/704-binary-search.cpp
@@ -1,12 +1,11 @@
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
+        ios_base::sync_with_stdio(false);
+        cin.tie(0);
+        cout.tie(0);
         int start = 0;
         int end = nums.size()-1;
-        if(nums.size()==1){
-            if(nums[0]==target) return 0;
-            else return -1;
-        }
         while(start<=end){
             int mid = start + (end - start)/2;
             if(nums[mid] == target){
