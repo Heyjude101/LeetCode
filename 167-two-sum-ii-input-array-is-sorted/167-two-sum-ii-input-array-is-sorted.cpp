c7553b74b234aafaeb49a28d93c5ade2dd6a1489
Ritik raj <ritikraj2884@gmail.com>
2022-04-06 23:10:57 +0530

    Time: 26 ms (19.29%), Space: 15.6 MB (75.54%) - LeetHub

---
+++ 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        //two pointers
+        int i = 0;
+        int j = nums.size()-1;
+        while(i<j){
+            int curSum = nums[i] + nums[j];
+            if(curSum == target ){
+                return {i+1 , j+1};                
+            }
+            else if(curSum < target){
+                i++;
+            }
+            else{
+                j--;
+            }
+        }
+        return {};
+    }
+};
+//fast io
+static bool     _foo = ios::sync_with_stdio(false);
+static ostream* _bar = cin.tie(NULL);
