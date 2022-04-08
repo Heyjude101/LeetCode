93e10f1352586196922616b429cc3f6f0a139c1e
Ritik raj <ritikraj2884@gmail.com>
2022-04-08 13:01:50 +0530

    Time: 0 ms (100.00%), Space: 5.7 MB (95.38%) - LeetHub

---
+++ 69-sqrtx/69-sqrtx.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int mySqrt(int x) {
+        long long left = 1;
+        long long right = (long long) x;
+        if(x==0) return 0;
+        while(left+1<right){
+            long long mid = (left + right)/2;
+            if(mid*mid == x){
+                return mid;
+            }
+            else if(mid*mid < x){
+                left = mid;
+            }
+            else{
+                right  = mid-1;
+            }
+        }
+        if(right*right<=x) return right;
+         return left;
+    }
+};
