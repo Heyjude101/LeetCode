ad6c7444e82ac966e928b6bcb493ded4c5eb72a0
Ritik raj <ritikraj2884@gmail.com>
2022-04-09 20:46:16 +0530

    Time: 28 ms (28.24%), Space: 13.7 MB (41.62%) - LeetHub

--- 347-top-k-frequent-elements/347-top-k-frequent-elements.cpp
+++ 347-top-k-frequent-elements/347-top-k-frequent-elements.cpp
@@ -12,10 +12,10 @@ public:
         priority_queue<pair<int,int>> pq; 
         for(auto it: map){
             pq.push(make_pair(it.second, it.first));
-            if(pq.size() > (int)map.size() - k){
-                res.push_back(pq.top().second);
-                pq.pop();
-            }
+        }
+        while(k-- >0){
+            res.push_back(pq.top().second);
+            pq.pop();
         }
         return res;
     }
