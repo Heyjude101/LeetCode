6b0d1349b56e55d7ec2355103ebe2a41fd6c408a
Ritik raj <ritikraj2884@gmail.com>
2022-04-11 00:07:27 +0530

    Time: 4 ms (71.06%), Space: 5.9 MB (27.01%) - LeetHub

--- 441-arranging-coins/441-arranging-coins.cpp
+++ 441-arranging-coins/441-arranging-coins.cpp
@@ -5,30 +5,33 @@ static const int _ = []() {
 }();
 class Solution {
 public:
-    long long sum(long long n){
-        return (n * (n+1))/2;   
-    }
+    // long long sum(long long n){
+    //     return (n * (n+1))/2;   
+    // }
     int arrangeCoins(int n) {
-        if(n<=2) return 1;
-        long long l  =1;
-        long long r = n;
-        long long ans;
-        while(l<=r){
-            long long mid = l + (r-l)/2;
-            long long coinReq = (long long) sum(mid);
-            if(coinReq == n){
-                ans = mid;
-                break;
-            }
-            else if(coinReq > n){
-                r = mid - 1;
-            }
-            else{
-                ans = mid;
-                l = mid + 1;
-            }
-        }
-        return ans;
+        // if(n<=2) return 1;
+        return (long long)(sqrt(2* (long long)n + 0.25) - 0.5);
+//         if(n<=2) return 1;
+//         long long l  =1;
+//         long long r = n;
+//         long long ans;
+//         while(l<=r){
+//             long long mid = l + (r-l)/2;
+//             long long coinReq = (long long) sum(mid);
+//             if(coinReq == n){
+//                 ans = mid;
+//                 break;
+//             }
+//             else if(coinReq > n){
+//                 r = mid - 1;
+//             }
+//             else{
+//                 ans = mid;
+//                 l = mid + 1;
+//             }
+//         }
+//         return ans;
         
+//     }
     }
 };
