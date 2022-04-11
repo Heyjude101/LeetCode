4f4484eed37d4ec86d9b0336dfa2f7c85df8c192
Ritik raj <ritikraj2884@gmail.com>
2022-04-11 22:14:30 +0530

    Time: 9 ms (10.20%), Space: 8.4 MB (53.74%) - LeetHub

--- 1608-special-array-with-x-elements-greater-than-or-equal-x/1608-special-array-with-x-elements-greater-than-or-equal-x.cpp
+++ 1608-special-array-with-x-elements-greater-than-or-equal-x/1608-special-array-with-x-elements-greater-than-or-equal-x.cpp
@@ -1,21 +1,53 @@
+// class Solution {
+// public:
+//     int specialArray(vector<int>& nums) {
+        
+        
 class Solution {
 public:
-    int specialArray(vector<int>& nums) {
-        // sort(nums.begin() , nums.end());
-        int size = nums.size();
-        int count =0;
-        for(int i=1;i<=size;i++){
-            for(int j = 0; j<size;j++){
-                if(nums[j] >= i){
-                    count++;
-                }
+    int solve(vector<int>& nums,int val){
+        int count=0;
+        for(int i=0;i<nums.size();++i){
+            if(nums[i]>=val){
+                ++count;
             }
-            if(count == i) return count;
-            
-            count =0;
         }
-        
+        return count;
+    }
+    int specialArray(vector<int>& nums) {
+       int low=0,high=1000;
+        while(low<=high){
+            int mid=(low+high)/2;
+            int comp=solve(nums,mid);
+            if(comp==mid){
+                return comp;
+            }
+            if(comp>mid){
+                low=mid+1;
+            }
+            else{
+                high=mid-1;
+            }
+        }
         return -1;
-        
     }
 };
+        // sort(nums.begin() , nums.end());
+        //brute force
+//         int size = nums.size();
+//         int count =0;
+//         for(int i=1;i<=size;i++){
+//             for(int j = 0; j<size;j++){
+//                 if(nums[j] >= i){
+//                     count++;
+//                 }
+//             }
+//             if(count == i) return count;
+            
+//             count =0;
+//         }
+        
+//         return -1;
+        
+//     }
+// };
