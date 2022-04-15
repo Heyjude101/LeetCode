6531cf0895512b9a753cd25e8c6d120101929130
Ritik raj <ritikraj2884@gmail.com>
2022-04-16 02:06:22 +0530

    Time: 8 ms (68.44%), Space: 10.3 MB (45.84%) - LeetHub

--- 1346-check-if-n-and-its-double-exist/1346-check-if-n-and-its-double-exist.cpp
+++ 1346-check-if-n-and-its-double-exist/1346-check-if-n-and-its-double-exist.cpp
@@ -4,35 +4,11 @@ public:
         unordered_set<int> s;
         //one pass approach
         for(auto it: arr){
-            if(s.count(it*2) or (it%2 == 0 and (s.count(it/2)))){
+            if(s.count(it*2) || (it%2 == 0 && (s.count(it/2)))){
                 return true;
             }
             s.insert(it);
         }
         return false;
-        // two pass
-        /*
-        unordered_set<int> s;
-        int countx = 0;
-        //initialize set
-        for(auto it:  arr){
-            if(it==0){
-                countx++;
-            }
-            s.insert(it);
-        }
-        if(countx>=2) return true;
-        
-        for(auto it: arr){
-            if(countx==0 && s.count(it*2)){
-                return true;
-            }
-            if(countx==1 && it!= 0 &&  s.count(it*2)){
-                return true;
-            }
-        }
-        
-        return false;
-        */
     }
 };
