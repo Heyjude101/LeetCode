6a03f3e172159396921ef3342fe1a7ce8010d507
Ritik raj <ritikraj2884@gmail.com>
2022-04-16 00:10:44 +0530

    Time: 120 ms (75.07%), Space: 14.1 MB (99.67%) - LeetHub

--- 240-search-a-2d-matrix-ii/240-search-a-2d-matrix-ii.cpp
+++ 240-search-a-2d-matrix-ii/240-search-a-2d-matrix-ii.cpp
@@ -1,17 +1,26 @@
+auto speedup=[]()
+{
+    std::ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return nullptr;
+}();
 class Solution {
 public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
-        int i = matrix.size() -1;
+        int row = matrix.size();
+        int col = matrix[0].size();
+        int i = row -1;
         int j =0;
-        while(i>=0 && j< matrix[0].size()){
+        while(i>=0 && j<col){
             if(target == matrix[i][j]){
                 return true;
             }
             else if(target < matrix[i][j]){
-                i--;
+                --i;
             }
             else{
-                j++;
+                ++j;
             }
         }
         return false; 
