6e538e7a814c34f66ed0f0f7ebaaa3e126fe72d7
Ritik raj <ritikraj2884@gmail.com>
2022-04-15 23:51:46 +0530

    Time: 6 ms (48.89%), Space: 9.5 MB (53.10%) - LeetHub

--- 74-search-a-2d-matrix/74-search-a-2d-matrix.cpp
+++ 74-search-a-2d-matrix/74-search-a-2d-matrix.cpp
@@ -3,13 +3,22 @@ public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
         int row = matrix.size();
         int col = matrix[0].size();
-        
-        for(int i=0;i<row; i++){
-            if(binary_search(matrix[i].begin() , matrix[i].end() , target)){
-                return true;
+        for(int i=0;i<row;i++){
+            for(int j=0;j<col;j++){
+                if(matrix[i][j] == target){
+                    return true;
+                }
             }
         }
+        
         return false;
+        
+        // for(int i=0;i<row; i++){
+        //     if(binary_search(matrix[i].begin() , matrix[i].end() , target)){
+        //         return true;
+        //     }
+        // }
+        // return false;
         
     }
 };
