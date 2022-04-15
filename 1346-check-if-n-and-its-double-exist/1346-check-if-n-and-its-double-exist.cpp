34e61bb77808e9c42501b7d6aa286aa3a5d1f037
Ritik raj <ritikraj2884@gmail.com>
2022-04-16 02:05:08 +0530

    Time: 8 ms (68.44%), Space: 10.4 MB (35.03%) - LeetHub

--- 1346-check-if-n-and-its-double-exist/1346-check-if-n-and-its-double-exist.cpp
+++ 1346-check-if-n-and-its-double-exist/1346-check-if-n-and-its-double-exist.cpp
@@ -1,10 +1,3 @@
-auto speedup=[]()
-{
-    std::ios::sync_with_stdio(false);
-    cin.tie(nullptr);
-    cout.tie(nullptr);
-    return nullptr;
-}();
 class Solution {
 public:
     bool checkIfExist(vector<int>& arr) {
