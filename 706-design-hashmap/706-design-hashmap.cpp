672319633a6183bc8acd4fa1e3595a3ea32ca6ab
Ritik raj <ritikraj2884@gmail.com>
2022-04-22 12:03:08 +0530

    Time: 228 ms (36.02%), Space: 208.3 MB (14.49%) - LeetHub

--- 706-design-hashmap/706-design-hashmap.cpp
+++ 706-design-hashmap/706-design-hashmap.cpp
@@ -1,8 +1,15 @@
+auto speedup=[]()
+{
+    std::ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return nullptr;
+}();
 class MyHashMap {
 public:
     vector<int> mp;
     MyHashMap() {
-        mp.resize(1000010 , -1);
+        mp.resize(1000001 , -1);
     }
     
     void put(int key, int value) {
