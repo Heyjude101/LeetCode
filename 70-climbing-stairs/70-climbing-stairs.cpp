efc3576d848c6e9689bc863a7196bf2c17dbeedf
Ritik raj <ritikraj2884@gmail.com>
2022-07-13 11:26:38 +0530

    Time: 0 ms (100.00%), Space: 6 MB (56.42%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.cpp
+++ 70-climbing-stairs/70-climbing-stairs.cpp
@@ -1,34 +1,51 @@
 class Solution {
 public:
-     int f(int n){
-        int prev2 = 1;
-        int prev =2;
-        int ans = n;
-        for(int i=3; i<=n; i++){
-            ans = prev + prev2;
-            prev2 = prev;
-            prev = ans;
-            
-        }
-        return ans;
+    int climbStairs(int n){
+        vector<int> dp(50 , -1);
+        return climbStairs(dp , n);
     }
-    // int f(int n, array<int , 46> &dp){
-    //     dp[1] = 1;
-    //     dp[2] =2;
-    //     for(int i=3; i<=n; i++){
-    //         dp[i] = dp[i-1] + dp[i-2];
-    //     }
-    //     return dp[n];
-    // }
-    // int f(int n , array<int , 46> &dp){
-    //     if(n<=2) return n;
-    //     if(dp[n] != -1) return dp[n];
-    //     return dp[n] = f(n-1 , dp) + f(n-2 , dp);
-    // }
-    int climbStairs(int n) {
-        // array<int , 46> dp;
-        // dp.fill(-1);
-        // return f(n , dp);
-        return f(n);
+    int climbStairs(vector<int> &dp , int n) {
+        if(n==1) return 1;
+        if(n==0) return 1;
+        if(dp[n] != -1) {
+            return dp[n];
+        }
+        return dp[n] = climbStairs(dp,n-1) + climbStairs(dp,n-2);
     }
 };
+
+
+// class Solution {
+// public:
+//      int f(int n){
+//         int prev2 = 1;
+//         int prev =2;
+//         int ans = n;
+//         for(int i=3; i<=n; i++){
+//             ans = prev + prev2;
+//             prev2 = prev;
+//             prev = ans;
+            
+//         }
+//         return ans;
+//     }
+//     // int f(int n, array<int , 46> &dp){
+//     //     dp[1] = 1;
+//     //     dp[2] =2;
+//     //     for(int i=3; i<=n; i++){
+//     //         dp[i] = dp[i-1] + dp[i-2];
+//     //     }
+//     //     return dp[n];
+//     // }
+//     // int f(int n , array<int , 46> &dp){
+//     //     if(n<=2) return n;
+//     //     if(dp[n] != -1) return dp[n];
+//     //     return dp[n] = f(n-1 , dp) + f(n-2 , dp);
+//     // }
+//     int climbStairs(int n) {
+//         // array<int , 46> dp;
+//         // dp.fill(-1);
+//         // return f(n , dp);
+//         return f(n);
+//     }
+// };
