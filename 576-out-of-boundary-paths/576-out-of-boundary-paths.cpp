af354bc2d8f23e83c826188538b490c7243f5aff
Ritik raj <ritikraj2884@gmail.com>
2022-07-16 12:22:18 +0530

    Time: 111 ms (8.19%), Space: 104.6 MB (5.03%) - LeetHub

--- 576-out-of-boundary-paths/576-out-of-boundary-paths.cpp
+++ 576-out-of-boundary-paths/576-out-of-boundary-paths.cpp
@@ -1,5 +1,6 @@
 class Solution {
 public:
+    int mod = 1e9+7;
     int countWays(int m , int  n , int moves , int startR , int startC , vector<vector<vector<int>>> &dp){
         if(startR<0 or startC<0 or startR>m or startC>n){
             return 1;
@@ -10,11 +11,11 @@ public:
         if(dp[moves][startR][startC] != -1){
             return dp[moves][startR][startC];
         }
-        int up = countWays(m , n , moves-1 , startR-1 , startC , dp)%1000000007;
-        int down = countWays(m , n , moves-1 , startR+1 , startC , dp)%1000000007;
-        int left = countWays(m , n , moves-1 , startR , startC-1 , dp)%1000000007;
-        int right = countWays(m , n , moves-1 , startR , startC+1 , dp)%1000000007;
-        return dp[moves][startR][startC] = ((up+down)%1000000007+(left+right)%1000000007)%1000000007;
+        int up = countWays(m , n , moves-1 , startR-1 , startC , dp);
+        int down = countWays(m , n , moves-1 , startR+1 , startC , dp);
+        int left = countWays(m , n , moves-1 , startR , startC-1 , dp);
+        int right = countWays(m , n , moves-1 , startR , startC+1 , dp);
+        return dp[moves][startR][startC] = ((up%mod + down%mod)%mod + (left%mod + right%mod)%mod)%mod;
     } 
     int findPaths(int m, int n, int maxMove, int startRow, int startColumn) {
         vector<vector<vector<int>>> dp(51 , vector<vector<int>>(51 , vector<int>(51 , -1)));
