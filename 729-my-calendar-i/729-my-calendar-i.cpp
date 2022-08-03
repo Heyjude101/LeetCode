b10f000f05e35dd90f9a8b2a633a1c1034b68161
Ritik raj <ritikraj2884@gmail.com>
2022-08-03 08:54:15 +0530

    Time: 247 ms (23.44%), Space: 39.3 MB (23.89%) - LeetHub

---
+++ 729-my-calendar-i/729-my-calendar-i.cpp
@@ -0,0 +1,39 @@
+class MyCalendar {
+public:
+    map<int,int> mp;
+    bool flag;
+    MyCalendar() {
+        flag = false;
+    }
+    
+    bool book(int start, int end) {
+        if(flag == false){
+            mp[start] = end;
+            flag = true;
+            return true;
+        }
+        bool ret = true;
+        // bool ret2 = true;
+        //check the start
+        for(auto it: mp){
+            if(start >= it.second or end <= it.first){
+                
+            }
+            else{
+                ret = false;
+                break;
+            }
+        }
+        if(ret){
+            mp[start] = end;
+        }
+        
+        return ret;  
+    }
+};
+
+/**
+ * Your MyCalendar object will be instantiated and called as such:
+ * MyCalendar* obj = new MyCalendar();
+ * bool param_1 = obj->book(start,end);
+ */
