81ecde6abd07991d304e864e7230182914597054
Ritik raj <ritikraj2884@gmail.com>
2022-08-07 17:25:42 +0530

    Time: 31 ms (51.40%), Space: 19.5 MB (59.43%) - LeetHub

--- 169-majority-element/169-majority-element.cpp
+++ 169-majority-element/169-majority-element.cpp
@@ -1,6 +1,59 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
+        int curr = nums[0];
+        int c = 1;
+        for(int i =1;i<nums.size();i++){
+            if(nums[i] != curr){
+                c--;
+                if(c<=0) {
+                    curr  = nums[i];
+                    c=1;
+                }
+            }
+            else{
+                c++;
+            }
+        }
+        return curr;
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        //MOORE VOTING ALGO
+        
         // int ans, count=1,cur = nums[0];
         // for(int i=1;i<nums.size();i++){
         //     if(cur == nums[i]){
@@ -17,13 +70,16 @@ public:
         // if(count>=1){
         // ans = cur;
         // }
-        sort(nums.begin(), nums.end());
-        int ans = nums[int(nums.size()/2)];
+        
+        // SORT AND DISPLAY MIDDLE ELEMENT
+        
+//         sort(nums.begin(), nums.end());
+//         int ans = nums[int(nums.size()/2)];
         
         
         
         
-        return ans;
+//         return ans;
         
     }
 };
